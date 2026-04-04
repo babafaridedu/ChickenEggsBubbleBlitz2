1f129411411a0d18678c800a19f47e4d116e66d4
babafaridedu <babafaridedu@icloud.com>
2026-04-04 09:40:58 +0500

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -26,9 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, SkillzDelegate {
         
         // Initialize Skillz with Game ID 97952 AFTER window is visible
         // We'll use .sandbox for testing; change to .production when ready to publish
-        Skillz.skillzInstance().initWithGameId("97952", for: self, with: .sandbox, allowExit: false)
+        Skillz.skillzInstance().initWithGameId("97952", for: self, with: .production, allowExit: false)
         
-        print("Skillz SDK initialized in SANDBOX mode")
+        /// print("Skillz SDK initialized in SANDBOX mode")
         
         return true
     }
